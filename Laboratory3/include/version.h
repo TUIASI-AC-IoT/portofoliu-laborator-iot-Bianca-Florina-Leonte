7fb058ae857db261b3c6a853a4951e3182988c62
Bianca-Florina-Leonte <115210171+Bianca-Florina-Leonte@users.noreply.github.com>
2025-03-05 19:41:28 +0200

    Add files via upload

---
+++ Laboratory3/include/version.h
@@ -0,0 +1,10 @@
+
+#ifndef BUILD_NUMBER
+  #define BUILD_NUMBER "2"
+#endif
+#ifndef VERSION
+  #define VERSION "v0.1.2 - 2025-03-05 19:39:53.754731"
+#endif
+#ifndef VERSION_SHORT
+  #define VERSION_SHORT "v0.1.2"
+#endif
